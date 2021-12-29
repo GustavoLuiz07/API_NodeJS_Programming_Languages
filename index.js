741a045e5ab96167199a289b8f8e3664952a7971
Sir.GustavLouis <91021057+GustavoLuiz07@users.noreply.github.com>
2021-12-29 16:17:12 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,68 @@
+const express = require('express')
+
+const server = express()
+
+server.use(express.json())
+
+//Base de dados inicial para realização do crud:
+
+let languages = [
+    {
+        "name": "Python", 
+        "creator": "Guido van Rossum",
+        "year": "1991"
+    }
+]                                                              //id inicial = 0
+
+//Desenvolvendo método de requisição http GET - Linguagem por id
+
+server.get('/languages/:index', (req, res) => {
+    const { index } = req.params
+
+    return res.json(languages[index])
+})
+
+
+//Desenvolvendo método de requisição http GET - Pegar todos os dados/linguagens
+
+server.get('/languages', (req, res) => {
+    return res.json(languages)
+})
+
+
+//Desenvolvendo método de requisição http POST - Adicionar nova linguagem
+
+server.post('/languages', (req, res) => {
+    const { name, creator, year } = req.body
+    languages.push({name, creator, year})
+
+    return res.json(languages)
+})
+
+
+//Desenvolvendo método de requisição http PUT - Atualizar uma linguagem
+
+server.put('/languages/:index', (req, res) => {
+    const { index } = req.params
+    const { name, creator, year } = req.body 
+     
+    languages[index] = ({name, creator, year})
+
+    return res.json(languages)
+})
+
+
+//Desenvolvendo método de requisição http DELETE - Deletar uma linguagem
+
+server.delete('/languages/:index', (req, res) => {
+    const { index } = req.params
+    languages.splice(index, 1)
+    return res.json({message: "A linguagem foi deletada com sucesso!"})
+})
+
+
+//Porta do servidor para realização de crud:
+
+server.listen(3000, () => {
+    console.log('O servidor está rodando...')
+})
